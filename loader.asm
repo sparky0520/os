cca8250c63345cc890f0e24fd3fb60c431ea3776
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2025-10-07 05:39:10 +0530

    long mode and 1gb page support checks

--- loader.asm
+++ loader.asm
@@ -2,6 +2,21 @@
 [ORG 0x7e00]
 
 Start:
+    mov [DriveId],dl
+
+    ;testing if 0x80000001 value is supported
+    mov eax,0x80000000 ;asking cpu what is it's highest extended function
+    cpuid
+    cmp eax,0x80000001
+    jb NotSupported ;jump if below what we need
+
+    mov eax,0x80000001
+    cpuid
+    test edx,(1<<29) ;long mode bit
+    jz NotSupported
+    test edx,(1<<26) ;1GB page support bit
+    jz NotSupported
+
     mov ah,0x13
     mov al,1
     mov bx,0xa
@@ -10,9 +25,11 @@ Start:
     mov cx,MessageLen
     int 0x10
 
+NotSupported:
 End:
     hlt
     jmp End
 
-Message:    db "Loader starts"
+DriveId:    db 0
+Message:    db "long mode is supported"
 MessageLen: equ $-Message   ; dynamic length
